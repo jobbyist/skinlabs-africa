9145355424e5e41ddd4cb0e11aa5b4c779c2eef5
jobbyist <232170229+jobbyist@users.noreply.github.com>
2026-09-08 21:22:36 +0200

    Export 8 Sept wave-B Daily Skinny briefing index

--- src/data/dailySkinnyBriefings.ts
+++ src/data/dailySkinnyBriefings.ts
@@ -89,3 +89,4 @@ export const dailySkinnyBriefingsSept2026 = [
 
 export { dailySkinnyBriefingsSept082026 } from "./dailySkinnyBriefingsSept082026";
 export { dailySkinnyBriefingsSept092026 } from "./dailySkinnyBriefingsSept092026";
+export { dailySkinnyBriefingsSept08b2026 } from "./dailySkinnyBriefingsSept08b2026";
