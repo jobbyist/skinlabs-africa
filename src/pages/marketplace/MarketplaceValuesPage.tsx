b72c56c079b9fd9d50d9d4ed3c62159e78b1c52e
jobbyist <232170229+jobbyist@users.noreply.github.com>
2026-09-13 02:22:31 +0200

    Add MarketplaceBreadcrumbs to values listing page

--- src/pages/marketplace/MarketplaceValuesPage.tsx
+++ src/pages/marketplace/MarketplaceValuesPage.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import { MarketplaceHeader } from "@/components/marketplace/MarketplaceHeader";
 import { MobileBottomNav } from "@/components/marketplace/MobileBottomNav";
+import { MarketplaceBreadcrumbs } from "@/components/marketplace/MarketplaceBreadcrumbs";
 import { MarketplaceGrid } from "@/components/marketplace/MarketplaceGrid";
 import { findBySlug, values } from "@/data/marketplace/taxonomy";
 
@@ -18,6 +19,9 @@ export default function MarketplaceValuesPage() {
       </Helmet>
       <div className="min-h-screen bg-[#faf9f7] font-sans pb-24 lg:pb-16">
         <MarketplaceHeader />
+        <div className="max-w-lg lg:max-w-6xl mx-auto px-4 lg:px-8 pt-4">
+          <MarketplaceBreadcrumbs items={[{ label: "Values" }, { label }]} />
+        </div>
         <MarketplaceGrid heading={label} preset={{ type: "values", slug }} />
         <MobileBottomNav />
       </div>
